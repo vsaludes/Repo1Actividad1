9030c407232dc9de0988b6f8667f3fce98be3a81
vsaludes <vsaludesm87@gmail.com>
2023-02-06 23:48:32 +0100

    Subidos mis aficiones

--- ActividadUno/src/victor/Victor.java
+++ ActividadUno/src/victor/Victor.java
@@ -4,7 +4,7 @@ public class Victor {
 
 	public static void main(String[] args) {
 		System.out.println("Hola, soy Víctor");
-
+		System.out.println("Me gusta perseguir patos en el parque, tocar la guitarra y tomar café.");
 	}
 
 }
