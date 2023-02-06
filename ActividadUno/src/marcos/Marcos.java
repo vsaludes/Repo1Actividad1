7d03fee2646e5db210bd1cb952188054b67565a6
mirilius <mr.mirilius@gmail.com>
2023-02-06 22:17:07 +0100

    Paquete Marcos y clase Marcos creado

---
+++ ActividadUno/src/marcos/Marcos.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package marcos;
+
+/**
+ * @author Marcos
+ *
+ */
+public class Marcos {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+	
+		System.out.println("Hola, soy Marcos");
+
+	}
+
+}
