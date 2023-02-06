b9dd918a72c2636aee66dd7d0a8891762aa85ec7
mirilius <mr.mirilius@gmail.com>
2023-02-06 23:49:20 +0100

    Hobbies de Marcos añadidos

--- ActividadUno/src/marcos/Marcos.java
+++ ActividadUno/src/marcos/Marcos.java
@@ -15,6 +15,7 @@ public class Marcos {
 	public static void main(String[] args) {
 	
 		System.out.println("Hola, soy Marcos");
+		System.out.println("Mis hobbies: Programar, hacer deporte y cantar");
 
 	}
 
