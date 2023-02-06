24d1fa6f2a7c75c0d253503d4df02539f5884588
pcascant <ppcascant@gmail.com>
2023-02-06 23:52:37 +0100

    Añadidos 3 hobbies de Jose

--- ActividadUno/src/jose/Jose.java
+++ ActividadUno/src/jose/Jose.java
@@ -3,8 +3,8 @@ package jose;
 public class Jose {
 
 	public static void main(String[] args) {
-		System.out.println("Hola, soy Jose")
-		System.out.println("3 de mis hobbys son: jugar al futbol, la música y la montaña");;
+		System.out.println("Hola, soy Jose");
+		System.out.println("3 de mis hobbys son: jugar al futbol, la música y la montaña");
 
 	}
 
