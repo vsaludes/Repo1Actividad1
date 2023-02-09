b9016054be1ce6aaf5b6bce1fc280577202d08b8
pcascant <ppcascant@gmail.com>
2023-02-09 23:14:17 +0100

    Subo mis 3 comidas favoritas en la clase de Victor

--- ActividadUno/src/victor/Victor.java
+++ ActividadUno/src/victor/Victor.java
@@ -5,6 +5,7 @@ public class Victor {
 	public static void main(String[] args) {
 		System.out.println("Hola, soy Víctor");
 		System.out.println("Me gusta perseguir patos en el parque, tocar la guitarra y tomar café.");
+		System.out.println("Me gusta la tortilla, el arroz al horno y el chocolate");
 	}
 
 }
